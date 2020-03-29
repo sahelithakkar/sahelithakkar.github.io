96f813e25db1a3302c4946161f46718e185a83a2
sajneethakkar <curioust6@gmail.com>
2020-03-29 15:55:28 -0700

    onmouseenter instead of onclick

--- saheli.js
+++ saheli.js
@@ -2,3 +2,8 @@ function inverse() {
   document.getElementById("image").src = 'heynowinverse.jpg';
   document.getElementById("text").style.color = '#9fae22';
 }
+
+function original() {
+  document.getElementById("image").src = 'heynow.jpg';
+  document.getElementById("text").style.color = '#cf6e48';	
+}
