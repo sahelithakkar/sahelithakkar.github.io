f9bcaa3df73f46246984cc0c1d21f4f496b7f66b
sajneethakkar <curioust6@gmail.com>
2020-04-03 19:14:05 -0700

    inverse icons

--- saheli.js
+++ saheli.js
@@ -2,10 +2,15 @@ function inverse() {
   document.getElementById("image").src = 'heynowinverse.jpg';
   document.getElementById("text").style.color = '#9fae22';
   document.getElementById("menu").style.backgroundColor = '#9fae22';
+  document.getElementById("twitter").src = 'inverse_twitter.jpg';
+  document.getElementById("instagram").src = 'inverse_ig.jpg';
+
 }
 
 function original() {
   document.getElementById("image").src = 'heynow.jpg';
   document.getElementById("text").style.color = '#cf6e48';	
   document.getElementById("menu").style.backgroundColor = '#cf6e48';
+  document.getElementById("twitter").src = 'twitter.jpg';
+  document.getElementById("instagram").src = 'instagram.jpg';
 }
