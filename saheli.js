338837d7799487a022e5156b238693530fceb813
sajneethakkar <curioust6@gmail.com>
2020-04-04 00:19:10 -0700

    icon updates

--- saheli.js
+++ saheli.js
@@ -2,15 +2,18 @@ function inverse() {
   document.getElementById("image").src = 'heynowinverse.jpg';
   document.getElementById("text").style.color = '#9fae22';
   document.getElementById("menu").style.backgroundColor = '#9fae22';
+  document.getElementById("pictures").src = 'inverse_pictures.jpg';
   document.getElementById("twitter").src = 'inverse_twitter.jpg';
   document.getElementById("instagram").src = 'inverse_ig.jpg';
-
+  document.getElementById("aboutme").src = 'inverse_aboutme.jpg';
 }
 
 function original() {
   document.getElementById("image").src = 'heynow.jpg';
   document.getElementById("text").style.color = '#cf6e48';	
   document.getElementById("menu").style.backgroundColor = '#cf6e48';
+  document.getElementById("pictures").src = 'pictures.jpg';
   document.getElementById("twitter").src = 'twitter.jpg';
   document.getElementById("instagram").src = 'instagram.jpg';
+  document.getElementById("aboutme").src = 'aboutme.jpg';
 }
