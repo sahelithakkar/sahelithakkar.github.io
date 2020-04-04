2f4597cd115c25f45d23c8c81c05fe47f9b510b2
sajneethakkar <curioust6@gmail.com>
2020-04-03 18:18:55 -0700

    nav bar

--- saheli.js
+++ saheli.js
@@ -1,9 +1,11 @@
 function inverse() {
   document.getElementById("image").src = 'heynowinverse.jpg';
   document.getElementById("text").style.color = '#9fae22';
+  document.getElementById("menu").style.backgroundColor = '#9fae22';
 }
 
 function original() {
   document.getElementById("image").src = 'heynow.jpg';
   document.getElementById("text").style.color = '#cf6e48';	
+  document.getElementById("menu").style.backgroundColor = '#cf6e48';
 }
